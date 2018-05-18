c13f44735db0fe63de0ddacee047d4bd690c35fe
revolver <595009116@qq.com>
2018-05-18 19:08:10 +0800

    添加搜索

--- pages/list/list.js
+++ pages/list/list.js
@@ -4,92 +4,95 @@ const httpUtil = require('../../utils/httpUtil.js')
 const app = getApp()
 
 Page({
-  data: {
-      appConfig: getApp().globalData.config,
+    data: {
+        appConfig: getApp().globalData.config,
         listData: [],
-        listPic:[],
-        searchForm:{
-            pageNo:1
+        pageData: [],
+        searchForm: {
+            pageNo: 1,
+            pageNum: 0
         },
         userInfo: {},
-        loadMoreShow:false,
+        loadMoreShow: false,
         profileInfoBoxShow: false
-  },
-  toggleProfileInfoBox:function(){
-      this.setData({
-          profileInfoBoxShow:!this.data.profileInfoBoxShow
-      })
-  },
-  //事件处理函数
-  bindViewTap: function () {
+    },
+    toggleProfileInfoBox: function () {
+        this.setData({
+            profileInfoBoxShow: !this.data.profileInfoBoxShow
+        })
+    },
+    isPullDownRefresh: function () {
+        //是否下拉刷新
+        return this.data.searchForm.pageNo < this.data.searchForm.pageNum ? true : false
+    },
 
-  },
+    loadData: function (pageNo, complete) {
+        let self = this
+        httpUtil.get('/wwd/users', {
+            data: {
+                pageable: true,
+                pageNo: pageNo,
+                includePic: true,
+                gender: ''
+            },
+            success: function (response) {
 
-  loadData: function(pageNo,complete){
-      let self = this
-      httpUtil.get('/wwd/users', {
-          data: {
-              pageable: true,
-              pageNo: pageNo,
-              includePic: true,
-              gender:'female'
-          },
-          success: function (response) {
+                if (complete && typeof complete == 'function') {
+                    complete()
+                }
 
-              if (complete && typeof complete == 'function') {
-                  complete()
-              }
+                let content = response.data.data.content
+                if (content && content.length > 0) {
+                    if (pageNo > 1) {
+                        self.data.listData.push.apply(self.data.listData, content)
+                        self.setData({
+                            listData: self.data.listData
+                        })
+                    } else {
+                        self.setData({
+                            listData: content
+                        })
+                    }
+                    self.setData({
+                        loadMoreShow: false,
+                        'searchForm.pageNo': response.data.data.page.pageNo,
+                        'searchForm.pageNum': response.data.data.page.pageNum
+                    })
+                } else {
 
-              let content = response.data.data.content
-              let listPic = response.data.data.pic
-              if (content && content.length > 0){
-                  if (pageNo > 1) {
-                      self.data.listData.push.apply(self.data.listData, content)
-                      self.data.listPic.push.apply(self.data.listPic, listPic)
-                      self.setData({
-                          listData: self.data.listData,
-                          listPic: self.data.listPic
-                      })
-                  } else {
-                      self.setData({
-                          listData: content,
-                          listPic: listPic
-                      })
-                  }
-                  self.setData({
-                      loadMoreShow: false,
-                      'searchForm.pageNo': response.data.data.page.pageNo
-                  })
-              }else{
-
-                self.setData({
-                    loadMoreShow: true
+                    self.setData({
+                        loadMoreShow: true
+                    })
+                }
+            },
+            fail: function () {
+                wx.showModal({
+                    title: '提示',
+                    content: '暂时没有匹配的数据',
                 })
-
-              }
-
-          }
-      })
-  },
-  //下拉刷新
-  onPullDownRefresh: function () {
-
-      this.loadData(1, wx.stopPullDownRefresh)
-  },
-  onReachBottom: function(){
-      
-      let self = this
-      if (self.data.loadMoreShow){
-          return
-      }
-      self.setData({
-          loadMoreShow: true
-      })
-      this.loadData(this.data.searchForm.pageNo + 1)
-  },
-  onLoad: function () {
-
-      this.loadData(1)
-
-  }
+            }
+        })
+    },
+    //下拉刷新
+    onPullDownRefresh: function () {
+        if (this.isPullDownRefresh()) {
+            this.loadData(1, wx.stopPullDownRefresh)
+        }
+    },
+    onReachBottom: function () {
+        let self = this
+        if (self.data.loadMoreShow) {
+            return
+        }
+        if (this.isPullDownRefresh()) {
+            console.log("show")
+            self.setData({
+                loadMoreShow: true
+            })
+            this.loadData(this.data.searchForm.pageNo + 1)
+        }
+    },
+    onLoad: function () {
+        this.loadData(1)
+    }
 })
